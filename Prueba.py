77ca36aef88ec7947bb1b7317736eda5ff0cf6a8
Sebastian Velandia <jsvelandiab@uqvirtual.edu.co>
2021-03-08 18:24:25 -0500

    Modificacion
    esto es una prueba

--- Prueba.py
+++ Prueba.py
@@ -1,17 +1,24 @@
 class Prueba:
-    for i in range(1,3):
+    """for i in range(1,3):
         print("Aprendamos a contar los numeros: " + str(i))
     edad = 22
     fecha = 25
     edad, fecha = fecha,edad
     divisionEntera = 9//2
-    potencia = 2**6
-    print(divisionEntera,potencia)
+    potencia = 2**6"""
+    #print(divisionEntera,potencia)
     s = "& abc &"
-    print(s.strip("&"))
+    #print(s.strip("&"))
+
+    num1 = input("Ingrese numero 1 \n")
+    num2 = input("Ingrese numero 2 \n")
+    print(num1[-2:])
+    #try:
+        
 
 
     def mensajeBienvenida (nombre:str):
         print("Bienvenido a reparapp "+nombre+", le agradecemos que nos haya elegido")
-    mensajeBienvenida("LUIS")
+    nombre = input("Ingrese su nombre: ")
+    mensajeBienvenida(nombre)
 
